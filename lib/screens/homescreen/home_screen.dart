e95d541dcd5ff6e2802f84da21e9a18d46e946ad
NabajitBhadury <nabajitbhadury@gmail.com>
2024-08-24 17:33:10 +0530

    small bug fixed

--- lib/screens/homescreen/home_screen.dart
+++ lib/screens/homescreen/home_screen.dart
@@ -3,7 +3,7 @@ import 'package:attrack/bloc/auth_bloc/events/auth_event_logout.dart';
 import 'package:attrack/bloc/auth_bloc/events/auth_event_show_update_user_details.dart';
 import 'package:attrack/models/user_model.dart';
 import 'package:attrack/screens/homescreen/all_meetings.dart';
-import 'package:attrack/screens/homescreen/notification-screen.dart';
+import 'package:attrack/screens/homescreen/notification_screen.dart';
 import 'package:attrack/screens/homescreen/upcomming_meetings_view.dart';
 import 'package:attrack/screens/homescreen/user_details_qr_view.dart';
 import 'package:attrack/services/firestore_storage/db_model.dart';
