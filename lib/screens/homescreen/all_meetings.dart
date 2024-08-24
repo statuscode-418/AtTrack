8497fb0c069facc8a05632a9b47de2ed0e7cde12
NabajitBhadury <nabajitbhadury@gmail.com>
2024-08-24 22:13:55 +0530

    edit profile implemented

--- lib/screens/homescreen/all_meetings.dart
+++ lib/screens/homescreen/all_meetings.dart
@@ -13,24 +13,37 @@ class AllMeetings extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(10.0),
-      child: ListView(
-        children: const [
-          Text(
-            'Event History',
-            style: TextStyle(
-              fontSize: 30,
-              color: Color(0xFF73FBFD),
-              fontWeight: FontWeight.bold,
+      child: Stack(
+        children: [
+          Positioned.fill(
+            child: ListView(
+              children: const [
+                Text(
+                  'Event History',
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Color(0xFF73FBFD),
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 20),
+                EventCard(
+                  imageUrl: 'https://via.placeholder.com/100',
+                  heading: 'Diversion',
+                  subheading: '36 hour Hackathon',
+                  date: 'Saturday 04 February',
+                  location: 'IEM Kolkata',
+                ),
+              ],
             ),
           ),
-          SizedBox(height: 20),
-          EventCard(
-            imageUrl: 'https://via.placeholder.com/100',
-            heading: 'Diversion',
-            subheading: '36 hour Hackathon',
-            date: 'Saturday 04 February',
-            location: 'IEM Kolkata',
-          ),
+          if (user.isAdmin)
+            Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: ElevatedButton(
+                    onPressed: () {}, child: const Text('Create Meeting')))
         ],
       ),
     );
