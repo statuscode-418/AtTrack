4f7b1611d2d06db492223dc04d3d25be15b852c3
Rohan-450 <zombiecpp106@gmail.com>
2024-08-25 01:09:11 +0530

    Event Details Screen added

--- lib/components/eventcard.dart
+++ lib/components/eventcard.dart
@@ -1,85 +1,107 @@
+import 'package:attrack/models/event_model.dart';
+import 'package:attrack/screens/eventdetails_screen/eventdetails_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class EventCard extends StatelessWidget {
-  final String heading;
-  final String date;
-  final String location;
-  final String subheading;
-  final String imageUrl;
+  final EventModel event;
 
   const EventCard({
     super.key,
-    required this.date,
-    required this.location,
-    required this.imageUrl,
-    required this.heading,
-    required this.subheading,
+    required this.event,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: const Color(0xFF322C2C),
-       margin: const EdgeInsets.all(8.0),
-      child: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: Row(
-          children: [
-            Expanded(
-              flex: 2,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    heading,
-                    style: const TextStyle(
-                      fontSize: 25,
-                      color: Color(0xFF73FBFD),
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const SizedBox(height: 5),
-                  Text(
-                    subheading,
-                    style: const TextStyle(
-                      fontSize: 20,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                  Text(
-                    date,
-                    style: const TextStyle(
-                      fontSize: 18,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Text(
-                    location,
-                    style: const TextStyle(
-                      fontSize: 18,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
+    var format = DateFormat('dd-MM-yyyy');
+    return GestureDetector(
+      onTap: (){
+        EventDetailsScreen(event: event);
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => EventDetailsScreen(
+              event: event,
             ),
-            const SizedBox(width: 8),
-            Container(
-              width: 120,
-              height: 120,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8.0),
-                image: DecorationImage(
-                  image: NetworkImage(imageUrl),
-                  fit: BoxFit.cover,
+          ),
+        );
+      },
+      child: Card(
+        color: const Color(0xFF322C2C),
+         margin: const EdgeInsets.all(8.0),
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Row(
+            children: [
+              Expanded(
+                flex: 2,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      event.title,
+                      style: const TextStyle(
+                        fontSize: 25,
+                        color: Color(0xFF73FBFD),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 5),
+                    Text(
+                      event.description ?? '',
+                      style: const TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      'Date: ${format.format(event.date)}',
+                      style: const TextStyle(
+                        fontSize: 18,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      event.address,
+                      style: const TextStyle(
+                        fontSize: 18,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ),
-          ],
+              const SizedBox(width: 8),
+              Container(
+                width: 120,
+                height: 120,
+                foregroundDecoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8.0),
+                  image: event.photoUrl != null ?  DecorationImage(
+                    image: NetworkImage(event.photoUrl!),
+                    fit: BoxFit.cover,
+                  ) : null,
+                ),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8.0),
+                  color: Colors.grey[300],
+                ),
+                child: event.photoUrl == null
+                    ?  Center(
+                        child: Icon(
+                          Icons.image,
+                          color: Colors.grey[800],
+                          size: 50,
+                        ),
+                      )
+                    : null,
+              ),
+            ],
+          ),
         ),
       ),
     );
